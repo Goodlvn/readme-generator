4d4ab90b8265bf06e440cdb78b7c07a26feb2a0e
JD Martinez <focus4ursoul@gmail.com>
2020-10-09 19:45:56 -0700

    add axios git hub call functionality

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# title
+
+[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]()
+
+## I am a mark down file
+
+test weeee!!
+
+also my name is Jonathan-David Martinez!
+
+## github profile picture
+![prof pic](https://avatars3.githubusercontent.com/u/37821521?v=4=250x)
+
--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const fs = require("fs");
-const util = require('util');
+const util = require("util");
+const axios = require("axios");
 const inquirer = require("inquirer");
+const { log } = require("console");
+
 
 const writeFileAsync = util.promisify(fs.writeFile); 
+
 
 function promptQuestions () {
     return inquirer.prompt ([
         {
             type: "ipnut",
-            message: "what is your name?",
+            message: "what is your Github username?",
             name: "username",
         },
     ]);
 };
 
-function generateMD (answers) {
+function generateMD (data) {
 
 let newMD = 
+
 `
 # title
+
+[![NPM Version](https://img.shields.io/npm/v/npm.svg?style=flat)]()
+
 ## I am a mark down file
 
 test weeee!!
 
-also my name is ${answers.username}!
+also my name is ${data.gitName}!
+
+## github profile picture
+![prof pic](${data.avatar}=250x)
 `
 
 return newMD;
@@ -31,10 +42,39 @@ return newMD;
 };
 
 async function createMDFile (){
-    let answers = await promptQuestions();
-    let markDown = await generateMD(answers);
+    try {
+        let answers = await promptQuestions();
 
-    writeFileAsync("README.md", markDown, "utf8");
+
+        //using spread operator 
+        let data = {...answers};
+    
+        console.log("answers", data);
+    
+    
+        const queryUrl = "https://api.github.com/users/" + data.username;
+        
+        await axios 
+                .get(queryUrl)
+                .then(function(r){
+                    console.log(r.data.name);
+            
+                    data.gitName = r.data.name;
+            
+                    data.avatar = r.data.avatar_url;
+            
+                });
+    
+        console.log(data);
+    
+    
+    
+        let markDown = await generateMD(data);
+    
+       await writeFileAsync("README.md", markDown, "utf8");
+    } catch(err) {
+        console.log(err);
+    }
 };
 
 createMDFile();
